31497ecaf6ad1a479871ae857f5f69f3a7cae36b
Djalma Henrique <djalmahenry@outlook.com>
2022-05-02 13:58:01 -0300

    feat: add home informations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NbStatusService } from '@nebular/theme';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [NbStatusService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,11 +4,15 @@ import { PagesComponent } from './pages.component';
 import { HomeComponent } from './home/home.component';
 import { PokedexComponent } from './pokedex/pokedex.component';
 import { PagesRoutingModule } from './pages-routing.module';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { NbIconModule } from '@nebular/theme';
 
 @NgModule({
   imports: [
     CommonModule,
-    PagesRoutingModule
+    PagesRoutingModule,
+    NbIconModule,
+    NbEvaIconsModule
   ],
   declarations: [PagesComponent, HomeComponent, PokedexComponent]
 })
